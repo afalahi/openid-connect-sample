cfa9ca230134904015f2943afc03fb0810d825ca
Ali Falahi <ali@afalahi.com>
2021-11-14 09:40:49 -0700

    Add files

---
+++ app.js
@@ -0,0 +1,114 @@
+require('dotenv').config()
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const session = require('express-session');
+const passport = require('passport');
+const { Issuer, Strategy } = require('openid-client');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+Issuer.discover(
+  process.env.IDC_TENANT //your IDC cloud am Oauth2 endpoint
+).then((forgerock) => {
+  const client = new forgerock.Client({
+    client_id: process.env.CLIENT_ID, //client ID for the express app client
+    client_secret: process.env.CLIENT_SECRET, //client Secret for the express app client
+    redirect_uris: [`${process.env.REDIRECT_URL}/auth/callback`], //your sign in, callback or redirect url.
+    post_logout_redirect_uris: [`${process.env.REDIRECT_URL}/logout/callback`],
+    token_endpoint_auth_method: 'client_secret_post', //make sure your client in IDC matches this setting. By default IDC uses basic auth for client authentication
+  });
+
+  app.use(
+    session({
+      secret: 'keyboard cat',
+      resave: false,
+      saveUninitialized: true,
+    })
+  );
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  passport.use(
+    'oidc',
+    new Strategy({ client }, (tokenSet, userinfo, done) => {
+      return done(null, tokenSet);
+    })
+  );
+
+  //handles serialization and deserialization of authenticated user
+  passport.serializeUser(function (user, done) {
+    done(null, user);
+  });
+  passport.deserializeUser(function (user, done) {
+    done(null, user);
+  });
+
+  // start authentication request
+  app.get('/auth', (req, res, next) => {
+    passport.authenticate('oidc', { scope: 'openid profile email' })(
+      req,
+      res,
+      next
+    );
+  });
+
+  // authentication callback
+  app.get('/auth/callback', (req, res, next) => {
+    passport.authenticate('oidc', {
+      successRedirect: '/users',
+      failureRedirect: '/',
+    })(req, res, next);
+  });
+
+  app.use('/users', usersRouter);
+
+  // start logout request
+  app.get('/logout', (req, res) => {
+    res.redirect(client.endSessionUrl());
+  });
+
+  // logout callback
+  app.get('/logout/callback', (req, res) => {
+    // clears the persisted user from the local storage
+    req.logout();
+    // redirects the user to a public route
+    res.redirect('/');
+  });
+
+  // catch 404 and forward to error handler
+  app.use(function (req, res, next) {
+    next(createError(404));
+  });
+
+  // error handler
+  app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+});
+
+module.exports = app;
---
+++ routes/users.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
+const jwtDecode = require('jwt-decode');
+
+router.get('/', ensureLoggedIn('/'), (req, res) => {
+  const decodedUser = {
+    access_token: {...jwtDecode(req.user.access_token)},
+    refresh_token: jwtDecode(req.user.refresh_token),
+    id_token: jwtDecode(req.user.id_token)
+  };
+  console.log(decodedUser);
+  res.render('users', { user: req.user, decodedUser });
+});
+
+module.exports = router;
